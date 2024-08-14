538200aba72b5574153ecb0eda5a4cffefbef15b
Sergey Moskalchuk <66484544+MOSKALCHUK31@users.noreply.github.com>
2024-08-14 03:37:47 +0300

    feat: add css modules

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Counter } from './components/Counter'
+import './index.scss'
+
+const App = () => {
+    return (
+        <div className={'app'}>
+            dasdsadsadsa
+            <Counter />
+        </div>
+    )
+}
+
+export default App
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,15 +1,14 @@
 import React, {useState} from 'react'
-import './Counter.scss'
+import classes from './Counter.module.scss'
 
 export const Counter = () => {
     const [count, setCount] = useState(0)
-
     const increment = () => setCount(count + 1)
 
     return (
         <div>
-            <h1>{ count }</h1>
-            <button onClick={increment}>click</button>
+            <h1>{count}</h1>
+            <button className={classes.btn} onClick={increment}>click</button>
         </div>
     )
 }
