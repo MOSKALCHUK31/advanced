e45b107039b62eee4927fc133d9277b6d2340cb0
Sergey Moskalchuk <66484544+MOSKALCHUK31@users.noreply.github.com>
2024-08-14 06:53:33 +0300

    feat: routing, code-splitting | lazy-loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import React from 'react'
-import { Counter } from './components/Counter'
+import React, {Suspense} from 'react'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import './index.scss'
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 
 const App = () => {
     return (
-        <div className={'app'}>
-            dasdsadsadsa
-            <Counter />
-        </div>
+        <Suspense fallback={'loading ...'}>
+            <BrowserRouter>
+                <div className="app">
+                    <Link to={'/'}>MAIN</Link>
+                    <Link to={'/about'}>ABOUT</Link>
+                    <Routes>
+                        <Route path={'/'} element={<MainPageAsync/>}/>
+                        <Route path={'/about'} element={<AboutPageAsync/>}/>
+                    </Routes>
+                </div>
+            </BrowserRouter>
+        </Suspense>
     )
 }
 
---
+++ src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AboutPage = () => {
+    return (
+        <div>
+            about view = ...
+        </div>
+    )
+}
+
+export default AboutPage
