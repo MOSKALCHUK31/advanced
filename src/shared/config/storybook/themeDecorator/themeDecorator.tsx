cab734f097d14310b2bfeb791b7b7f3093f80786
Sergey Moskalchuk <66484544+MOSKALCHUK31@users.noreply.github.com>
2024-10-10 12:18:59 +0300

    remode ref loki

--- src/shared/config/storybook/themeDecorator/themeDecorator.tsx
+++ src/shared/config/storybook/themeDecorator/themeDecorator.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 import { StoryFn } from '@storybook/react'
 import { Theme, ThemeProvider } from 'app/providers/ThemeProvider'
 
-export const themeDecorator = (theme: Theme) => (Story: StoryFn) => (
+export const themeDecorator = (theme: Theme) => (StoryComponent: StoryFn) => (
     <ThemeProvider initialTheme={theme}>
         <div className={`app ${theme}`}>
-            <Story />
+            <StoryComponent />
         </div>
     </ThemeProvider>
 )
